128ee8eb5a5ed86e9c9fa95de3ebef79ce9a2e9d
Bruno Ventura Gross <bvgross@gmx.com>
2025-05-25 21:57:20 -0300

    configurando

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -1,12 +1,23 @@
 return {
   {
-    "bvgross/neovim-ayu-adapted",
-    name = "ayu",
+    'bvgross/neovim-ayu-adapted',
+    name = 'ayu',
     priority = 1000,
     config = function(plugin, opts)
-      require('ayu').setup({
+      require('ayu').setup {
         mirage = true,
-      })
+      }
+    end,
+  },
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = true,
+    opts = ...,
+    config = function(plugin, opts)
+      require('gruvbox').setup {
+        contrast = 'hard',
+      }
     end,
   },
 }
