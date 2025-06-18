dd2e764ff7038cf3dd1f669d14afaf3aa6a4423a
Bruno Ventura Gross <bvgross@gmx.com>
2025-06-18 16:40:06 -0300

    substituí telescope com fzf-lua

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -3,6 +3,26 @@ return {
     'folke/noice.nvim',
     event = 'VeryLazy',
     opts = {
+      messages = {
+        view = 'mini', -- default view for messages
+        view_error = 'mini', -- view for errors
+        view_warn = 'mini', -- view for warnings
+        view_history = 'messages', -- view for :messages
+        view_search = 'virtualtext', -- view for search count messages. Set to `false` to disable
+      },
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+        },
+        hover = {
+          view = 'hover',
+        },
+      },
+      presets = {
+        lsp_doc_border = true,
+      },
       -- add any options here
     },
     dependencies = {
@@ -11,7 +31,6 @@ return {
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      'rcarriga/nvim-notify',
     },
   },
 }
